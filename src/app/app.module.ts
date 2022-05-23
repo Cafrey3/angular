7f49d0b8bdf63b135acd2833ced8af5b9fcda3f3
Cafrey3 <tokar9980@gmail.com>
2022-05-23 04:05:54 +0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {UsersComponent} from './components/users/users.component';
+import {PostsComponent} from './components/posts/posts.component';
+import {CommentsComponent} from './components/comments/comments.component';
+import {UserComponent} from './components/user/user.component';
+import {PostComponent} from './components/post/post.component';
+import {CommentComponent} from './components/comment/comment.component';
+import {HomeComponent} from './components/home/home.component';
+import {HttpClientModule} from "@angular/common/http";
+import {RouterModule} from "@angular/router";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    PostsComponent,
+    CommentsComponent,
+    UserComponent,
+    PostComponent,
+    CommentComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'home-page', component: HomeComponent},
+      {path: 'users-page', component: UsersComponent},
+      {path: 'posts-page', component: PostsComponent},
+      {path: 'comments-page', component: CommentsComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/components/post/post.component.ts
@@ -0,0 +1,21 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+import {IPost} from "../../interfaces/IPost";
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+
+  @Input()
+  posts:IPost
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
